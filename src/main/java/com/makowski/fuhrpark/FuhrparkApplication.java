772b9da0457ceaed739590e320ff124188d16c8b
Michal <115497445+Michal-Makowski@users.noreply.github.com>
2023-02-08 19:01:48 +0100

    Add files via upload

--- src/main/java/com/makowski/fuhrpark/FuhrparkApplication.java
+++ src/main/java/com/makowski/fuhrpark/FuhrparkApplication.java
@@ -9,7 +9,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
-
 import com.makowski.fuhrpark.entity.Car;
 import com.makowski.fuhrpark.entity.Cost;
 import com.makowski.fuhrpark.entity.CostType;
@@ -32,55 +31,55 @@ public class FuhrparkApplication implements CommandLineRunner {
 	}
 
 	@Bean
-			public BCryptPasswordEncoder bCryptPasswordEncoder() {
-			return new BCryptPasswordEncoder();
-		}	
+	public BCryptPasswordEncoder bCryptPasswordEncoder() {
+		return new BCryptPasswordEncoder();
+	}
 
 	@Override
 	public void run(String... args) throws Exception {
-		Car[] cars = new Car[]{
-			new Car("A-XA544", "Audi", "A4", LocalDate.parse("2022-01-22")),
-			new Car("A-DA778", "BMW", "3", LocalDate.parse("2021-04-27")),
-			new Car("M-SE484", "Mercedes", "E-Klasse", LocalDate.parse("2020-03-12")),
-			new Car("F-WE098", "BMW", "X3", LocalDate.parse("2022-10-11"))
+		Car[] cars = new Car[] {
+				new Car("A-XA544", "Audi", "A4", LocalDate.parse("2022-01-22")),
+				new Car("A-DA778", "BMW", "3", LocalDate.parse("2021-04-27")),
+				new Car("M-SE484", "Mercedes", "E-Klasse", LocalDate.parse("2020-03-12")),
+				new Car("F-WE098", "BMW", "X3", LocalDate.parse("2022-10-11"))
 		};
-		
-		for(int i = 0; i < cars.length; i++){
+
+		for (int i = 0; i < cars.length; i++) {
 			carRepository.save(cars[i]);
 		}
 
-		CostType[] costTypes = new CostType[]{
-			new CostType("Tanken"),
-			new CostType("Service")
+		CostType[] costTypes = new CostType[] {
+				new CostType("Tanken"),
+				new CostType("Service")
 		};
 
-		for(int i = 0; i < costTypes.length; i++){
+		for (int i = 0; i < costTypes.length; i++) {
 			costTypeRepository.save(costTypes[i]);
 		}
 
-	 	Cost[] costs = new Cost[]{
-			new Cost(72.58, 526, LocalDate.parse("2022-02-15"), "Tanken"),
-			new Cost(70.78, 1002, LocalDate.parse("2022-02-25"), "Tanken"),
-			new Cost(69.53, 1528, LocalDate.parse("2022-03-07"), "Tanken"),
-			new Cost(75.59, 2088, LocalDate.parse("2022-03-17"), "Tanken"),
-			new Cost(73.14, 2601, LocalDate.parse("2022-03-29"), "Tanken")
+		Cost[] costs = new Cost[] {
+				new Cost(72.58, 526, LocalDate.parse("2022-02-15"), "Tanken"),
+				new Cost(70.78, 1002, LocalDate.parse("2022-02-25"), "Tanken"),
+				new Cost(69.53, 1528, LocalDate.parse("2022-03-07"), "Tanken"),
+				new Cost(75.59, 2088, LocalDate.parse("2022-03-17"), "Tanken"),
+				new Cost(73.14, 2601, LocalDate.parse("2022-03-29"), "Tanken")
 		};
 
-		for(int i = 0; i < costs.length; i++){
+		for (int i = 0; i < costs.length; i++) {
 			costs[i].setCar(cars[0]);
-			costs[i].setCostType(costTypes[1]);
+			costs[i].setCostType(costTypes[0]);
 			costRepository.save(costs[i]);
 		}
 
-		Cost[] costs1 = new Cost[]{
-			new Cost(62.58, 505, LocalDate.parse("2021-02-15"), "Tanken"),
-			new Cost(60.78, 1012, LocalDate.parse("2021-02-25"), "Tanken"),
-			new Cost(59.53, 1499, LocalDate.parse("2021-03-07"), "Tanken"),
-			new Cost(65.59, 2064, LocalDate.parse("2021-03-17"), "Tanken"),
-			new Cost(63.14, 2633, LocalDate.parse("2021-03-29"), "Tanken")
+		Cost[] costs1 = new Cost[] {
+				new Cost(62.58, 505, LocalDate.parse("2021-02-15"), "Tanken"),
+				new Cost(60.78, 1012, LocalDate.parse("2021-02-25"), "Tanken"),
+				new Cost(59.53, 1499, LocalDate.parse("2021-03-07"), "Tanken"),
+				new Cost(65.59, 2064, LocalDate.parse("2021-03-17"), "Tanken"),
+				new Cost(63.14, 2633, LocalDate.parse("2021-03-29"), "Tanken")
 		};
 
-		for(int i = 0; i < costs1.length; i++){
+		for (int i = 0; i < costs1.length; i++) {
 			costs1[i].setCar(cars[1]);
 			costs1[i].setCostType(costTypes[0]);
 			costRepository.save(costs1[i]);
