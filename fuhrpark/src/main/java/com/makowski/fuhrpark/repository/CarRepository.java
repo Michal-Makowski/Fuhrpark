b9f537a7324353efe547ecd67656b3dd53c145a1
Michal <115497445+Michal-Makowski@users.noreply.github.com>
2023-02-08 17:49:10 +0100

    Delete fuhrpark directory

--- fuhrpark/src/main/java/com/makowski/fuhrpark/repository/CarRepository.java
+++
@@ -1,9 +0,0 @@
-package com.makowski.fuhrpark.repository;
-
-import org.springframework.data.repository.CrudRepository;
-
-import com.makowski.fuhrpark.entity.Car;
-
-public interface CarRepository extends CrudRepository<Car, Long>{
-    
-}
--- fuhrpark/src/main/java/com/makowski/fuhrpark/repository/CostTypeRepository.java
+++
@@ -1,9 +0,0 @@
-package com.makowski.fuhrpark.repository;
-
-import org.springframework.data.repository.CrudRepository;
-
-import com.makowski.fuhrpark.entity.CostType;
-
-public interface CostTypeRepository extends CrudRepository<CostType, Long>{
-    
-}
